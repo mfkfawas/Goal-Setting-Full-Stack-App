57c05240f46ec1c7a87c295ddd468de15d1e9dfd
mfkfawas <mfkfawas@gmail.com>
2022-02-22 13:49:18 +0530

    Cleared Deploy Bug

--- backend/app.js
+++ backend/app.js
@@ -11,6 +11,24 @@ app.use(express.urlencoded({ extended: false }));
 app.use('/api/goals', goalRouter);
 app.use('/api/users', userRouter);
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(
+      path.resolve(
+        __dirname,
+        '../',
+        'frontend',
+        'build',
+        'index.html'
+      )
+    );
+  });
+} else {
+  app.get('/', (req, res) => res.send('Please set to production.'));
+}
+
 //This will override the default express error handler.
 app.use(errorHandler);
 
--- backend/server.js
+++ backend/server.js
@@ -18,23 +18,7 @@ const connectDB = async () => {
 };
 connectDB();
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/build')));
 
-  app.get('*', (req, res) => {
-    res.sendFile(
-      path.resolve(
-        __dirname,
-        '../',
-        'frontend',
-        'build',
-        'index.html'
-      )
-    );
-  });
-} else {
-  app.get('/', (req, res) => res.send('Please set to production.'));
-}
 
 const port = process.env.PORT || 5000;
 
